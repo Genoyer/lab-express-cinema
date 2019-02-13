008d970ea1641da7c47c0509c9642cadefca848b
genoyer <lcrimg@gmail.com>
2019-02-13 13:58:50 -0500

    done

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,31 @@
 const express = require('express');
 const router  = express.Router();
+const Movie = require('../models/movie.js');
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
+});
+
+router.get('/movies', (req, res, next) => {
+  Movie.find()
+    .then((movies) => {
+      res.render('movies', { movies })
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+});
+
+router.get('/movies/:id', (req, res, next) => {
+  let movieId = req.params.id;
+  Movie.findOne({'_id': movieId})
+    .then((movie) => {
+      res.render('movie-details', { movie })
+    })
+    .catch((error) => {
+      console.log(error)
+    })
 });
 
 module.exports = router;
